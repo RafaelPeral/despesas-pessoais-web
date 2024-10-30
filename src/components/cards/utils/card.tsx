2cfe93e0c7ac7e268b301cf1e3aaf17b24e7a1fd
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-10-30 20:49:12 -0300

    adding conponents shadcn

---
+++ src/components/cards/utils/card.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react'
+import { useItem } from '@/hooks/app'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+
+
+export const Carde: React.FC<{ content: ReactElement, index: number }> = React.memo(({ content, index }) => {
+    const { deleteItem } = useItem()
+    return (
+        <Card>
+            {content}
+            <CardFooter>
+                <button onClick={() => deleteItem(index)}>deletar</button>
+            </CardFooter>
+        </Card>
+    )
+})
