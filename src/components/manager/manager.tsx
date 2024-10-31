fba594e03e7bec02a683e235b789dcd11b0fc4e1
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-10-31 07:07:37 -0300

    Update manager.tsx

--- src/components/manager/manager.tsx
+++ src/components/manager/manager.tsx
@@ -4,7 +4,7 @@ import { Carde } from '../cards/utils/card'
 export default function Manager() {
     const { item } = useItem()
     return (
-      <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
+      <div className='w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
         {item.map((item, index) => (
           <Carde content={item} index={index}/>
         ))}
