8b9d2aa5d943be99ef5f2e139a3b0ca176c44c16
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-11-06 01:25:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,13 @@ export default defineConfig({
     },
   },
   server: {
-    port: 10000,
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
     strictPort: true,
   },
 })
